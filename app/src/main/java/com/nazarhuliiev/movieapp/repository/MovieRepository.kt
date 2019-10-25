b349fc6e2f36eb43dbef27617bb51dc7007b589a
Nazar Huliiev <nazar.huliev@gmail.com>
2019-10-25 11:29:44 +0300

    Update mocked data

--- app/src/main/java/com/nazarhuliiev/movieapp/repository/MovieRepository.kt
+++ app/src/main/java/com/nazarhuliiev/movieapp/repository/MovieRepository.kt
@@ -12,14 +12,14 @@ class MovieRepository {
     fun getAllMovies() : MutableLiveData<List<LocalMovie>> {
 
         val movies = listOf(
-            LocalMovie("Movie 1", 1, 5f),
-            LocalMovie("Movie 1", 1, 5f),
-            LocalMovie("Movie 1", 1, 5f),
-            LocalMovie("Movie 1", 1, 5f),
-            LocalMovie("Movie 1", 1, 5f),
-            LocalMovie("Movie 1", 1, 5f),
-            LocalMovie("Movie 1", 1, 5f),
-            LocalMovie("Movie 1", 1, 5f));
+            LocalMovie("Movie 1", 1991, 5f),
+            LocalMovie("Movie 2", 1992, 1f),
+            LocalMovie("Movie 3", 1993, 3f),
+            LocalMovie("Movie 4", 1994, 5f),
+            LocalMovie("Movie 5", 1995, 1f),
+            LocalMovie("Movie 6", 1996, 3f),
+            LocalMovie("Movie 7", 1997, 5f),
+            LocalMovie("Movie 8", 1998, 5f));
 
         return MutableLiveData(movies);
     }
