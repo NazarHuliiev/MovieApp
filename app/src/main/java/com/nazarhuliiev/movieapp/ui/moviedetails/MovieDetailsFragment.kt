4f57e9b6315e25ad11c55f3a58d83c9751d5b83b
Nazar Huliiev <nazar.huliev@gmail.com>
2020-02-23 00:14:02 +0200

    Add map view on details page

--- app/src/main/java/com/nazarhuliiev/movieapp/ui/moviedetails/MovieDetailsFragment.kt
+++ app/src/main/java/com/nazarhuliiev/movieapp/ui/moviedetails/MovieDetailsFragment.kt
@@ -1,29 +1,69 @@
 package com.nazarhuliiev.movieapp.ui.moviedetails
 
+import android.location.Geocoder
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.transition.TransitionInflater
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.nazarhuliiev.movieapp.GlideApp
 import com.nazarhuliiev.movieapp.R
 import kotlinx.android.synthetic.main.fragment_movie_details.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class MovieDetailsFragment: Fragment(R.layout.fragment_movie_details) {
+class MovieDetailsFragment: Fragment(R.layout.fragment_movie_details), OnMapReadyCallback {
 
     private val args: MovieDetailsFragmentArgs by navArgs()
     private val viewModel by viewModel<MovieDetailsViewModel>()
+    private lateinit var mapView: MapView
+    private lateinit var geocoder: Geocoder
+    private lateinit var countryName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
         sharedElementReturnTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
         setHasOptionsMenu(true)
+
+        geocoder = Geocoder(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView = super.onCreateView(inflater, container, savedInstanceState)
+
+        mapView = rootView!!.findViewById<MapView>(R.id.movie_details_map)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+
+        try {
+            MapsInitializer.initialize(context)
+        } catch (ex: Exception ) {
+
+        }
+
+        mapView.getMapAsync(this)
+
+        return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,6 +84,21 @@ class MovieDetailsFragment: Fragment(R.layout.fragment_movie_details) {
         })
     }
 
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
     private fun loadDetailsImage() {
         val imageUrl = args.posterPath
         movie_details_image.also {
@@ -61,5 +116,29 @@ class MovieDetailsFragment: Fragment(R.layout.fragment_movie_details) {
             android.R.id.home -> findNavController().navigateUp()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        searchCountry(p0!!)
+    }
+
+    private fun searchCountry(map: GoogleMap) {
+        viewLifecycleOwner.lifecycleScope.launch {
+
+            val addresses = withContext(Dispatchers.IO) {
+                geocoder.getFromLocationName(countryName, 1)
+            }
+
+            if(addresses != null) {
+                val address = addresses[0]
+                val point = LatLng(address.latitude, address.longitude)
+
+                map!!.addMarker(MarkerOptions().position(point).title(address.featureName).snippet("Description" +
+                        "some"))
+
+                val cameraPosition = CameraPosition.Builder().target(point).zoom(5f).build()
+                map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+            }
+        }
     }
 }
